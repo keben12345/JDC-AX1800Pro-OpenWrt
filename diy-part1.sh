d920c84b3b2f15425c770b973a79520ab54451f3
keben12345 <79047578+keben12345@users.noreply.github.com>
2024-08-24 07:11:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,6 @@
 # Add a feed source
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git;master' >> feeds.conf.default
