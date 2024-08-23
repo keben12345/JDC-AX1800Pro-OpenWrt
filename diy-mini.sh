0b33f98026f8b710e9be2d0c1ef8c5456f49f1bb
keben12345 <79047578+keben12345@users.noreply.github.com>
2024-08-24 06:18:38 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -20,7 +20,11 @@ function git_sparse_clone() {
 # xunlei
 # git clone --depth 1 --branch v3.11.2-32 https://github.com/0x676e67/thunder.git package/xunlei
 git clone --depth 1 https://github.com/animegasan/luci-app-quickstart package/luci-app-quickstart
-
+# passwall
+#git clone --depth 1 --branch main https://github.com/xiaorouji/openwrt-passwall-packages.git 
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # 修改版本为编译日期
 date_version=$(date +"%y.%m.%d")
